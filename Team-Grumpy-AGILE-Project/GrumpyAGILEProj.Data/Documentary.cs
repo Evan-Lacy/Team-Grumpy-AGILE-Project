81530e2813dc48408f6aa0b1b47be335186871aa
carltgoss <ctgoss1840@gmail.com>
2020-11-17 10:33:08 -0500

    DataClasses Added

---
+++ Team-Grumpy-AGILE-Project/GrumpyAGILEProj.Data/Documentary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrumpyAGILEProj.Data
+{
+    public class Documentary
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public MaturityRating MaturityRating { get; set; }
+        [Required]
+        public DocGenre DocGenre { get; set; }
+        [Required]
+        public double RunTime { get; set; }
+
+    }
+
+    public enum DocGenre
+    {
+        Expository = 1,
+        Poetic,
+        Essayistic,
+        Observational,
+        Participatory,
+        Performance,
+        Interview,
+        Dramatization,
+        Mixed,
+        Animation
+    }
+}
