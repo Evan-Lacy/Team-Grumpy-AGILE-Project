bbdb7b0785172b1ea53c79deadd25bb488f556e2
carltgoss <ctgoss1840@gmail.com>
2020-11-17 14:52:00 -0500

    TVShows create, post, list by title

---
+++ Team-Grumpy-AGILE-Project/GrumpyAGILEProj.Models/TVShowCreate.cs
@@ -0,0 +1,27 @@
+﻿using GrumpyAGILEProj.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrumpyAGILEProj.Models
+{
+    public class TVShowCreate
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+        [Required]
+        public TVMaturityRating TVMaturityRating { get; set; }
+        [Required]
+        public double AverageEpisodeLength { get; set; }
+        public double TotalEpisodes { get; set; }
+        public double TotalSeasons { get; set; }
+
+    }
+}
